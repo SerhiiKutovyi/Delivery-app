59805de905f61c0058b75d4035db584eb80bce9a
SerhiiKutovyi <109694632+SerhiiKutovyi@users.noreply.github.com>
2023-06-18 13:13:04 +0300

    Initial commit

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import { Btn } from './Button.styled';
 
 const Button = ({ onClick }) => {
-  return <Btn onClick={onClick}>add to Cart</Btn>;
+  return <Btn onClick={onClick}>Add to Cart</Btn>;
 };
 
 export default Button;
